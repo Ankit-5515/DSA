a30148b15faa56fe85cb6d76f60c326b630bf991
Ankit <y24170510@dhsgsu.edu.in>
2026-08-22 13:01:21 +0530

    Time: 0 ms (100%), Space: 63.6 MB (10.9%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+
+        int n = arr.size();
+
+        int st = 1 , end = n-2;
+
+        while(st <= end){
+            int mid = st + (end - st)/2;
+
+            if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(arr[mid-1]< arr[mid]){    // increasing side
+                st = mid + 1;
+            }else{       // decreasing side
+                end = mid-1;
+            }
+        }
+        return -1;   
+    }
+};
