e11857a17940aeac686fcec3321ca6f1da733dd7
Ankit <y24170510@dhsgsu.edu.in>
2026-08-24 23:50:58 +0530

    Time: 4 ms (30.47%), Space: 8.5 MB (64.88%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        long long rev = 0 ;
+        int digit;
+        int n = x;
+
+        while( n > 0){
+            digit = n % 10;
+            rev = rev * 10 + digit;
+            n =n /10;
+        }
+
+        if(rev == x){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+};
