87b7079076cb50235b10f365807776546c970b27
Ankit <y24170510@dhsgsu.edu.in>
2026-08-25 16:40:55 +0530

    Time: 0 ms (100%), Space: 7.6 MB (88.72%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        if(n == 0){
+            return 0;
+        }
+
+        int a = 0;
+        int b = 1;
+        int next = 0;
+
+        for(int i=1; i<n; i++){
+
+            next = a + b;
+            a = b;
+            b = next; 
+
+        }
+
+        return b;
+        
+    }
+};
