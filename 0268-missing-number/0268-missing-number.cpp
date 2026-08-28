7ea56fa1dce3e0123a6d50a1e443855cd56c4a45
Ankit <y24170510@dhsgsu.edu.in>
2026-08-28 11:49:27 +0530

    Time: 0 ms (100%), Space: 21.7 MB (93.92%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+        int n = nums.size();
+        int i;
+        sort(nums.begin(), nums.end());
+
+        for( i = 0; i<n ; i++){
+
+            if(nums[i] != i ){
+                return i;
+            }else{
+            
+            }
+
+        }
+        return i++;
+        
+    }
+};
